123b3b21503f067aac006230f0354fcefdc7598b
alexsmaldone <alexsmaldone@gmail.com>
2022-06-10 17:49:02 -0400

    added redis connection

--- inventory/main.py
+++ inventory/main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+from redis_om import get_redis_connection, HashModel
 
 app = FastAPI()
+redis = get_redis_connection(
+  host="redis-14476.c14.us-east-1-3.ec2.cloud.redislabs.com:14476",
+  port=14476,
+  password="rs69yioSEvrpqkmjHJGFQuavL4YrCGf4",
+  decode_responses=True
+)
 
+class Product(HashModel):
+  name: str
+  price: float
+  quantity: int
 
 @app.get("/")
 async def root():
