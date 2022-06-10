71510d5d771a06eba56421bff93d9c11d5e459ea
alexsmaldone <alexsmaldone@gmail.com>
2022-06-10 18:10:07 -0400

    produc tpost request working

--- inventory/main.py
+++ inventory/main.py
@@ -5,11 +5,14 @@ from redis_om import get_redis_connection, HashModel
 app = FastAPI()
 
 app.add_middleware(
-  CORSMiddleware
+  CORSMiddleware,
+  allow_origins=['http://localhost:3000'],
+  allow_methods=['*'],
+  allow_headers=['*']
 )
 
 redis = get_redis_connection(
-  host="redis-14476.c14.us-east-1-3.ec2.cloud.redislabs.com:14476",
+  host="redis-14476.c14.us-east-1-3.ec2.cloud.redislabs.com",
   port=14476,
   password="rs69yioSEvrpqkmjHJGFQuavL4YrCGf4",
   decode_responses=True
@@ -26,3 +29,7 @@ class Product(HashModel):
 @app.get('/products')
 def all():
   return Product.all_pks()
+
+@app.post('/products')
+def create(product: Product):
+  return product.save()
