f2cfe656ee5fd87134ba6d82e898df10c0bc961e
alexsmaldone <alexsmaldone@gmail.com>
2022-06-11 23:28:17 -0400

    refunds for order error

--- inventory/consumer.py
+++ inventory/consumer.py
@@ -16,10 +16,12 @@ while True:
       for result in results:
         obj = result[1][0][1]
         product = Product.get(obj['product_id'])
-        print(product)
-        product.quantity = product.quantity - int(obj['quantity'])
-        product.save()
-
+        if product:
+          print(product)
+          product.quantity = product.quantity - int(obj['quantity'])
+          product.save()
+        else:
+          redis.xadd('refund', obj, '*')
   except Exception as e:
     print(str(e))
 
