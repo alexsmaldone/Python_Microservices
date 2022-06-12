217ba2f8df27462df5a580cf607fec597c02400d
alexsmaldone <alexsmaldone@gmail.com>
2022-06-12 13:04:32 -0400

    added route for product componenet

--- inventory-frontend/src/App.js
+++ inventory-frontend/src/App.js
@@ -1,8 +1,15 @@
 import Products from "./components/Products";
 import logo from "./logo.svg";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  return <Products />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Products />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
