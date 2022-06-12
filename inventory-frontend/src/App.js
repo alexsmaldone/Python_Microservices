acc6acd9358b20603a951cadb1de413f9bb5f570
alexsmaldone <alexsmaldone@gmail.com>
2022-06-12 12:56:04 -0400

    craeted wrapper component

--- inventory-frontend/src/App.js
+++ inventory-frontend/src/App.js
@@ -1,43 +1,8 @@
+import Products from "./components/Products";
 import logo from "./logo.svg";
 
 function App() {
-  return (
-    <div className="App">
-      <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-        <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6" href="#">
-          Company name
-        </a>
-        <div class="navbar-nav">
-          <div class="nav-item text-nowrap">
-            <a class="nav-link px-3" href="#">
-              Sign out
-            </a>
-          </div>
-        </div>
-      </header>
-
-      <div class="container-fluid">
-        <div class="row">
-          <nav
-            id="sidebarMenu"
-            class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse"
-          >
-            <div class="position-sticky pt-3">
-              <ul class="nav flex-column">
-                <li class="nav-item">
-                  <a class="nav-link active" aria-current="page" href="#">
-                    Products
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </nav>
-
-          <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4"></main>
-        </div>
-      </div>
-    </div>
-  );
+  return <Products />;
 }
 
 export default App;
