cc6759c1862beba59ef5f39f009e3989d372b282
alexsmaldone <alexsmaldone@gmail.com>
2022-06-12 13:24:32 -0400

    form inputs for product create

--- inventory-frontend/src/components/ProductsCreate.js
+++ inventory-frontend/src/components/ProductsCreate.js
@@ -1,22 +1,41 @@
 import React from "react";
 import Wrapper from "./Wrapper";
+import { useState } from "react";
 
 function ProductsCreate() {
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+  const [quantity, setQuantity] = useState("");
+
   return (
     <Wrapper>
       <form className="mt-3">
         <div className="form-floating pb-3">
-          <input className="form-control" placeholder="Name" />
+          <input
+            className="form-control"
+            placeholder="Name"
+            onChange={(e) => setName(e.target.value)}
+          />
           <label>Name</label>
         </div>
 
         <div className="form-floating pb-3">
-          <input className="form-control" placeholder="Price" />
+          <input
+            type="number"
+            className="form-control"
+            placeholder="Price"
+            onChange={(e) => setPrice(e.target.value)}
+          />
           <label>Price</label>
         </div>
 
         <div className="form-floating pb-3">
-          <input className="form-control" placeholder="Quantity" />
+          <input
+            type="number"
+            className="form-control"
+            placeholder="Quantity"
+            onChange={(e) => setQuantity(e.target.value)}
+          />
           <label>Quantity</label>
         </div>
 
